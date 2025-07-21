5f7ee4b0e0273b15e3b696e0f4eef39b4a8e7840
s-eunbi-kim <86344770+s-eunbi-kim@users.noreply.github.com>
2025-07-21 13:59:55 +0900

    Create calc.h

---
+++ src/calc.h
@@ -0,0 +1,16 @@
+#ifndef HIKER_INCLUDED
+#define HIKER_INCLUDED
+#include <stdbool.h>
+#include <stddef.h>
+
+int answer(void);
+bool isPositive(int);
+int add(int, int);
+int subtract(int, int);
+int divide(int, int);
+int multiply(int, int);
+bool isEven(int);
+// void greet(const char*, char*, size_t);
+const char* greet(const char*);
+
+#endif
